0fbc5d6df80d41d664cda2ae04e7b53dfb04e98f
Jonathan Pardo <al02778355@tecmilenio.mx>
2017-11-18 16:01:45 -0600

    Iniciando con la configuración

--- src/fbcmd4j/Utils.java
+++ src/fbcmd4j/Utils.java
@@ -1,5 +1,6 @@
 package fbcmd4j;
 
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,11 +12,18 @@ import java.util.function.BiConsumer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.auth.AccessToken;
+
+
+
+
 public class Utils {
 	static final Logger logger = LogManager.getLogger(Utils.class);
-	
-	
-	
+
 	
 	public static Properties loadPropertiesFromFile(String foldername, String filename) throws IOException{
 		Properties props = new Properties();
@@ -43,8 +51,19 @@ public class Utils {
 		return props;
 	}
 	
-	static void ConfigurarToken(String foldername, String filename, Properties props, Scanner scanner) {
-	//cration
-	}
+	static void ConfigurarToken(String foldername, String filename, Properties props, Scanner scanner) throws FacebookException {
+		Facebook facebook = new FacebookFactory().getInstance();
+		facebook.setOAuthAppId(props.getProperty("oauth.appId"), props.getProperty("oauth.appSecret"));
+		facebook.setOAuthPermissions(props.getProperty("oauth.permissions"));
+		facebook.setOAuthAccessToken(new AccessToken(props.getProperty("oauth.accessToken"), null));
+		
+		System.out.println(props.getProperty("oauth.appId"));
+		System.out.println(props.getProperty("oauth.appSecret"));
+		System.out.println(props.getProperty("oauth.permissions"));
+		System.out.println(props.getProperty("oauth.accessToken"));
+		
+		facebook.postStatusMessage("Gaara del Desierto creo que ya logre hacer algo ");
 
+	}
+	
 }
